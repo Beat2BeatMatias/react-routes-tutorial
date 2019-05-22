8d96a0d50bc645b5627e1b6a7016e3590ef60e4e
Matias Mirko <mmffalkiewicz@gmail.com>
2019-05-21 23:04:26 -0300

    initial commit

--- src/users.js
+++ src/users.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-const User = ({ match }) => <p>{match.params.id}</p>
+const User = ({ match }) => <p>{match.params.id}</p>;
 class Users extends React.Component {
     render() {
         const { url } = this.props.match;
